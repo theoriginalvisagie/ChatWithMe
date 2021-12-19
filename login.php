bac16ddd582af0a0a89d7b57b686c35eb3977757
theoriginalvisagie <67701001+theoriginalvisagie@users.noreply.github.com>
2021-12-19 09:02:04 +0200

    jquery added

--- login.php
+++ login.php
@@ -3,11 +3,7 @@
         <link rel="stylesheet" href="Libraries/Bootstrap/bootstrap-5.1.3-dist/css/bootstrap.min.css"/>
         <script src="Libraries/Bootstrap/bootstrap-5.1.3-dist/js/bootstrap.min.js"></script>
         <script src="script/js.js?v=2"></script>
-        <script
-        src="https://code.jquery.com/jquery-3.6.0.js"
-        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
-        crossorigin="anonymous">
-        </script>
+        <script src="script/jquery.js?v=2"></script>
 
 
     <body>
--- script/js.js
+++ script/js.js
@@ -1,7 +1,18 @@
 function registerNewUser(){
     // alert("yeas");
-    var queryString = $('#registerUser').formSerialize();
+    var data = $('#registerUser').serialize();
+
     console.log(queryString);
+
+    $.ajax(   // request url
+    {
+        url: "ajax/ajaxData.php",
+        type: 'post',
+        data: ({'action':"registerNewUser",data:"registerNewUser"}),
+        success: function (data, status, xhr) {// success callback function
+            $('p').append(data);
+    }
+});
 }
 
 function registerNewUser2(){
